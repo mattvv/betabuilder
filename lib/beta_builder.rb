422a3323117588272073b9ffa9e8c53a0423bc3c
Matt Van Veenendaal <m@mattvv.com>
2012-12-03 14:11:54 -0800

    more attempts at codesigning

--- lib/beta_builder.rb
+++ lib/beta_builder.rb
@@ -140,7 +140,8 @@ module BetaBuilder
           FileUtils.mkdir_p("pkg/Payload")
           FileUtils.mv(@configuration.built_app_path, "pkg/Payload/#{@configuration.app_file_name}")
           Dir.chdir("pkg") do
-            system("zip -r '#{@configuration.ipa_name}' Payload")
+            system("/usr/bin/xcrun -sdk iphoneos PackageApplication -v #{@@configuration.built_app_path} -o #{@configuration.ipa_name} --sign \"iPhone Distribution: #{@configuration.company_name}\"")
+            # system("zip -r '#{@configuration.ipa_name}' Payload")
           end
           FileUtils.mkdir('pkg/dist')
           FileUtils.mv("pkg/#{@configuration.ipa_name}", "pkg/dist")
